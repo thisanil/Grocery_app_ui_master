651ff4790a056644727f86460fdb56d7d02e4367
thisanil <an011saini@gmail.com.com>
2023-09-20 15:22:47 +0530

    add file

---
+++ lib/models/product.dart
@@ -0,0 +1,21 @@
+class Product {
+  final String? title, image;
+
+  Product({this.title, this.image});
+}
+
+List<Product> demo_products = [
+  Product(title: "Cabbage", image: "assets/img_1.png"),
+  Product(title: "Broccoli", image: "assets/img_2.png"),
+  Product(title: "Carrot", image: "assets/img_3.png"),
+  Product(title: "Pakcoy", image: "assets/img_4.png"),
+  Product(title: "Cucumber", image: "assets/img_1.png"),
+  Product(title: "lettuce Leaf", image: "assets/pngegg.png"),
+  Product(title: "Potatoe", image: "assets/potato.png"),
+  Product(title: "Coriander", image: "assets/dhniya.png"),
+  Product(title: "Cauliflower Cabbage", image: "assets/gobi.png"),
+  Product(title: "Capsicum", image: "assets/shimla.png"),
+  Product(title: "Tomato", image: "assets/tomato1.png"),
+
+
+];
